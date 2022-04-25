3faafecbdecac6ca625321929b9f76dec1533901
patrick0314 <patrick0011223344@gmail.com>
2022-04-25 22:23:21 +0800

    2022.04.25 22.23

--- hw3.py
+++ hw3.py
@@ -1,32 +1,34 @@
-import math
+import time
 import wave
 import numpy as np
 import simpleaudio as sa
+import matplotlib.pyplot as plt
 
 def getmusic(score, beat, name, key=0, unit_beat=1, fs=44100):
-    # Semitone p = 69 + 12*log(2, f/f0)
-    p = [60, 62, 64, 65, 67, 69, 71, 72, 74, 76, \
-            77, 79, 81, 83, 84, 86, 88, 89, 91, 93, 95, 96]
-
     # Base Frequency of Each Tone
-    # f = f0 * 2^((p-69)/12)
-    freq = [440.00, 493.88, 523.25, 587.31, 659.26, 739.99, 830.61]
+    basis = [261.63, 261.63*2**(1/6), 261.63*2**(1/3), 261.63*2**(5/12) \
+                , 261.63*2**(7/12), 261.63*2**(3/4), 261.63*2**(11/12)]
+    freq_dif = {-1:2**(-1), -2:2**(-5/6), -3:2**(-2/3), -4:2**(-7/12), -5:2**(-5/12), -6:2**(-1/4), -7:2**(-1/12), \
+                1:2**(0), 2:2**(1/6), 3:2**(1/3), 4:2**(5/12), 5:2**(7/12), 6:2**(3/4), 7:2**(11/12), \
+                8:2**(1), 9:2**(7/6), 10:2**(4/3), 11:2**(17/12), 12:2**(19/12), 13:2**(7/4), 14:2**(23/12) }
 
     # Generation Music
     music = []
     for i in range(len(score)):
         if beat[i] > 1:
             for b in range(beat[i]):
-                t = np.linspace(0, 1, 1 * fs, False)
-                note = np.sin(freq[score[i]-1] * t * 2 * np.pi)
+                t = np.linspace(0, 1, fs, False)
+                f = basis[key] * freq_dif[score[i]]
+                note = np.sin(f * t * 2 * np.pi)
                 audio = note * (2**15 - 1) / np.max(np.abs(note))
                 audio = audio.astype(np.int16)
                 if b == 0: audio[:2050] = 0
-                elif b == beat[i]-1: audio[-4010:] = 0
+                elif b == beat[i]-1: audio[-2050:] = 0
                 music.append(audio)
         else:
             t = np.linspace(0, 1, fs, False)
-            note = np.sin(freq[score[i]-1] * t * 2 * np.pi)
+            f = basis[key] * freq_dif[score[i]]
+            note = np.sin(f * t * 2 * np.pi)
             audio = note * (2**15 - 1) / np.max(np.abs(note))
             audio = audio.astype(np.int16)
             audio[:2050] = 0
@@ -35,6 +37,18 @@ def getmusic(score, beat, name, key=0, unit_beat=1, fs=44100):
 
     music = np.array(music)
 
+    '''
+    # Sketch Plot
+    tmp = np.reshape(music, (music.shape[0]*music.shape[1], ))
+    time = np.arange(0, len(tmp)) # * 1 / fs
+    plt.plot(time, tmp)
+    plt.show()
+    plt.close()
+    '''
+
+    '''
+    '''
+    # Play Music
     play_obj = sa.play_buffer(music, 1, 2, fs)
     play_obj.wait_done()
 
@@ -54,7 +68,8 @@ if __name__ == '__main__':
     #   getmusic(score, beat, name) # Generate the music file 'twinkle.wav'
     # =========================================================================
 
-    score = [1, 1, 5, 5, 6, 6, 5]
-    beat = [1, 1, 1, 1, 1, 1, 2]
+    score = [-1, -1, -5, -5, -6, -6, -5, -4, -4, -3, -3, -2, -2, -1]
+    score *= -1
+    beat = [1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2]
     name = 'twinkle'
     getmusic(score, beat, name)
