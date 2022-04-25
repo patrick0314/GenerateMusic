2a33cf143a84a664fb8e9747904dd3e7a2fb3b97
patrick0314 <patrick0011223344@gmail.com>
2022-04-26 04:40:53 +0800

    2022.04.26 04.40

--- hw3.py
+++ hw3.py
@@ -1,64 +1,70 @@
 import time
 import wave
+import argparse
 import numpy as np
 import simpleaudio as sa
 import matplotlib.pyplot as plt
 
-def getmusic(score, beat, name, key=1, unit_beat=1, volume=1):
+def fading(X, order):
+    n = int(0.05 * len(X))
+    factor = np.linspace(0, 1, n) if order == 1 else np.linspace(1, 0, n)
+    if order == 1: X[:n] *= factor
+    else: X[-n:] *= factor
+    return X
+
+def getmusic(score, beat, name, key, unit_beat, volume, play):
     # Base Frequency of Each Tone
     fs = 96000
-    basis = [261.63, 261.63*2**(1/6), 261.63*2**(1/3), 261.63*2**(5/12) \
-                , 261.63*2**(7/12), 261.63*2**(3/4), 261.63*2**(11/12)]
-    freq_dif = {-1:2**(-1), -2:2**(-5/6), -3:2**(-2/3), -4:2**(-7/12), -5:2**(-5/12), -6:2**(-1/4), -7:2**(-1/12), \
-                1:2**(0), 2:2**(1/6), 3:2**(1/3), 4:2**(5/12), 5:2**(7/12), 6:2**(3/4), 7:2**(11/12), \
-                8:2**(1), 9:2**(7/6), 10:2**(4/3), 11:2**(17/12), 12:2**(19/12), 13:2**(7/4), 14:2**(23/12) }
+    basis = np.array([261.63, 261.63*2**(1/6), 261.63*2**(1/3), 261.63*2**(5/12) \
+                , 261.63*2**(7/12), 261.63*2**(3/4), 261.63*2**(11/12), 261.63*2])
+    keys = [1, 2**(1/6), 2**(1/3), 2**(5/12), 2**(7/12), 2**(3/4), 2**(11/12), 2]
 
     # Generation Music
     music = []
+    basis = basis * keys[key-1]
     for i in range(len(score)):
         if beat[i] > 1:
             for b in range(beat[i]):
                 t = np.linspace(0, unit_beat, unit_beat * fs, False)
-                f = basis[key-1] * freq_dif[score[i]]
+                f = basis[score[i]-1]
                 note = np.sin(f * t * 2 * np.pi)
                 audio = volume * 0.5 * note * (2**15 - 1) / np.max(np.abs(note))
-                audio = audio.astype(np.int16)
-                if b == 0: audio[:2050] = 0
-                elif b == beat[i]-1: audio[-2050:] = 0
-                music.append(audio)
+                if b == 0: fading(audio, 1)
+                elif b == beat[i]-1: fading(audio, 0)
+                music.append(audio.astype(np.int16))
         else:
             t = np.linspace(0, unit_beat, unit_beat * fs, False)
-            f = basis[key-1] * freq_dif[score[i]]
+            f = basis[score[i]-1]
             note = np.sin(f * t * 2 * np.pi)
             audio = volume * 0.5 * note * (2**15 - 1) / np.max(np.abs(note))
-            audio = audio.astype(np.int16)
-            audio[:2050] = 0
-            audio[-2050:] = 0
-            music.append(audio)
+            fading(audio, 1)
+            fading(audio, 0)
+            music.append(audio.astype(np.int16))
 
     music = np.array(music)
+    music = np.reshape(music, (music.shape[0]*music.shape[1], ))
 
     '''
     # Sketch Plot
-    tmp = np.reshape(music, (music.shape[0]*music.shape[1], ))
     time = np.arange(0, len(tmp)) # * 1 / fs
     plt.plot(time, tmp)
     plt.show()
     plt.close()
     '''
 
-    '''
     # Play Music
-    play_obj = sa.play_buffer(music, 1, 2, fs)
-    play_obj.wait_done()
-    '''
+    if play:
+        play_obj = sa.play_buffer(music, 1, 2, fs)
+        play_obj.wait_done()
 
-    f = wave.open(name+'.wav', 'wb')
-    f.setnchannels(2)
-    f.setsampwidth(2)
-    f.setframerate(fs)
-    f.writeframes(music.tobytes())
-    f.close()
+    # Save
+    if name:
+        f = wave.open(name+'.wav', 'wb')
+        f.setnchannels(2)
+        f.setsampwidth(2)
+        f.setframerate(fs)
+        f.writeframes(music.tobytes())
+        f.close()
 
 if __name__ == '__main__':
     # =========================================================================
@@ -69,31 +75,29 @@ if __name__ == '__main__':
     #   getmusic(score, beat, name) # Generate the music file 'twinkle.wav'
     # =========================================================================
 
-    print('\n =========================================================================')
-    print(' Basic Function:')
-    print('   score = [1, 1, 5, 5, 6, 6, 5] # 1:Do, 2:Re, 3:Mi ...')
-    print('   beat = [1, 1, 1, 1, 1, 1, 2] # 拍子')
-    print('   name = \'twinkle\'')
-    print('   getmusic(score, beat, name) # Generate the music file \'twinkle.wav\'')
-    print(' Advanced Function:')
-    print('   score -1:低音Do, -2:低音Re, -3:低音Mi, ..., 8:高音Do, 9:高音Re, 10:高音Mi, ... 包含上下八度音')
-    print('   key = [1, 2, 3, 4, 5, 6, 7] # 1:C major, 2:D major, 3:E major 更改 key 的話，score 對應的音調也會改變')
-    print('   unit beat 為每拍的單位時間')
-    print('   volume = 0 ~ 2, 可以調整音樂大小聲')
-    print(' =========================================================================\n')
-    
-    score = list(map(int, list(input('Enter the notion: '))))
-    beat = list(map(int, list(input('Enter the beats: '))))
-    if len(score) != len(beat):
+    # Data Argumentation
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--score', help='Enter the notion of the music.\n In C major, 1:Do 2:Re 3:Mi ...')
+    parser.add_argument('--beat', help='Enter the beat in the music')
+    parser.add_argument('--name', help='Enter the name of the file of the music')
+    parser.add_argument('--key', help='Enter the major of the music and will affect the score represenetation.\n \
+        (1, 2, 3, 4, 5, 6, 7, 8) = (C, D, E, F, G, A, B, C)')
+    parser.add_argument('--unit_beat', help='Enter the sec per beat.')
+    parser.add_argument('--volume', help='Enter the factor of the basic volume. Range is from 0 to 2')
+    parser.add_argument('--play', help='Determine whether play the music while executing the program. 1 for playing')
+    args = parser.parse_args()
+
+    # Fool Proof
+    if (not args.score or not args.beat) or len(args.score) != len(args.beat):
         print('ERROR !!!')
-    name = input('Enter the file name: ')
-    key = input('Enter wanted key: ')
-    if key == '': key = 1
-    else: key = int(key)
-    unit_beat = input('Enter wanted unit_beat: ')
-    if unit_beat == '': unit_beat = 1
-    else: unit_beat = int(unit_beat)
-    volume = input('Enter wanted volume: ')
-    if volume == '': volume = 1
-    else: volume = int(volume)
-    getmusic(score, beat, name, key, unit_beat, volume)
+    else:
+        # Data initialization
+        score = list(map(int, list(args.score)))
+        beat = list(map(int, list(args.beat)))
+        name = args.name
+        key = int(args.key) if args.key else 1
+        unit_beat = int(args.unit_beat) if args.unit_beat else 1
+        volume = float(args.volume) if args.volume else 1
+        play = 1 if args.play == '1' else 0
+        
+        getmusic(score, beat, name, key=key, unit_beat=unit_beat, volume=volume, play=play)
--- test.py
+++ test.py
@@ -10,8 +10,8 @@ def template():
     fs = wavefile.getframerate()
     num_frame = wavefile.getnframes()
 
-    #print('fs =', fs) # 44100
-    #print('num of frame =', num_frame) # 230400
+    print('fs =', fs) # 44100
+    print('num of frame =', num_frame) # 230400
 
     # 讀取波形與數據
     str_data = wavefile.readframes(num_frame)
@@ -19,37 +19,35 @@ def template():
     wave_data = wave_data / max(abs(wave_data))
     n_channel = 2
     wave_data = np.reshape(wave_data, (num_frame, n_channel))
-    #print(wave_data.shape) # (230400, 2)
-    #print(wave_data)
+    print(wave_data.shape) # (230400, 2)
+    print(wave_data)
 
     # 畫出音訊波形圖
     time = np.arange(0, num_frame) * 1 / fs
-    #plt.plot(time, wave_data)
-    #plt.show()
-    #plt.close()
+    plt.plot(time, wave_data)
+    plt.show()
+    plt.close()
 
     # 播放聲音
     n_bytes = 2
     wave_data = (2**15-1) * wave_data
     wave_data = wave_data.astype(np.int16)
-    #play_obj = sa.play_buffer(wave_data, n_channel, n_bytes, fs)
-    #play_obj.wait_done()
-    #print(wave_data.shape) # (230400, 2)
-    #print(wave_data)
+    play_obj = sa.play_buffer(wave_data, n_channel, n_bytes, fs)
+    play_obj.wait_done()
+    print(wave_data.shape) # (230400, 2)
+    print(wave_data)
 
     # 製作音檔
-    '''
     f = wave.open('test-copy.wav', 'wb')
     f.setnchannels(2)
     f.setsampwidth(2)
     f.setframerate(fs)
     f.writeframes(wave_data.tobytes())
     f.close()
-    '''
 
 def test():
     score = [1, 1, 5, 5, 6, 6, 5]
-    beat = [1, 1, 1, 1, 1, 1, 1]
+    beat = [1, 1, 1, 1, 1, 1, 2]
     freq = [440.00, 493.88, 523.25, 587.31, 659.26, 739.99, 830.61]
     fs = 44100
 
@@ -59,8 +57,6 @@ def test():
         note = np.sin(freq[score[i]-1] * t * 2 * np.pi)
         audio = note * (2**15 - 1) / np.max(np.abs(note))
         audio = audio.astype(np.int16)
-        audio[:2050] = 0
-        audio[-2050:] = 0
         music.append(audio)
 
     music = np.array(music)
